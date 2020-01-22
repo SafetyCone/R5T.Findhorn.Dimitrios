34084e1079874d5045b4d00cc7508dbe43c858a9
MinexAutomation <minex.automation@gmail.com>
2020-01-21 16:58:42 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Findhorn.Dimitrios.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Findhorn.Dimitrios.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ source/R5T.Findhorn.Dimitrios/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Findhorn.Dimitrios
+{
+    public class Class1
+    {
+    }
+}
