41a23a7bdbde05b1108a56dbe39822d9314268b5
MinexAutomation <minex.automation@gmail.com>
2020-01-21 17:06:47 -0800

    Added OrganizationDataDirectoryPathProvider implementation of IDataDirectoryPathProvider service.

--- source/R5T.Findhorn.Dimitrios/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Findhorn.Dimitrios
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Findhorn.Dimitrios/Code/Services/Implementations/OrganizationDataDirectoryPathProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using R5T.Dalkeith;
+using R5T.Dimitrios;
+using R5T.Lombardy;
+
+
+namespace R5T.Findhorn.Dimitrios
+{
+    public class OrganizationDataDirectoryPathProvider : IDataDirectoryPathProvider
+    {
+        private IOrganizationDirectoryPathProvider OrganizationDirectoryPathProvider { get; }
+        private IDataDirectoryNameConvention DataDirectoryNameConvention { get; }
+        private IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public OrganizationDataDirectoryPathProvider(
+            IOrganizationDirectoryPathProvider organizationDirectoryPathProvider,
+            IDataDirectoryNameConvention dataDirectoryNameConvention,
+            IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.OrganizationDirectoryPathProvider = organizationDirectoryPathProvider;
+            this.DataDirectoryNameConvention = dataDirectoryNameConvention;
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        public string GetDataDirectoryPath()
+        {
+            string organizationDirectoryPath = this.OrganizationDirectoryPathProvider.GetOrganizationDirectoryPath();
+
+            string dataDirectoryName = this.DataDirectoryNameConvention.GetDataDirectoryName();
+
+            string dataDirectoryPath = this.StringlyTypedPathOperator.GetDirectoryPath(organizationDirectoryPath, dataDirectoryName);
+            return dataDirectoryPath;
+        }
+    }
+}
