a60f8d4149eee3ae2be8b52c1635a8cc969bbecf
MinexAutomation <minex.automation@gmail.com>
2020-01-30 14:49:40 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Findhorn.Dimitrios/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Dalkeith;
+using R5T.Dimitrios;
+using R5T.Lombardy;
+
+
+namespace R5T.Findhorn.Dimitrios
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="OrganizationDataDirectoryPathProvider"/> implementation of <see cref="IDataDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddOrganizationDataDirectoryPathProvider(this IServiceCollection services,
+            ServiceAction<IOrganizationDirectoryPathProvider> addOrganizationDirectoryPathProvider,
+            ServiceAction<IDataDirectoryNameConvention> addDataDirectoryNameConvention,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            services
+                .AddSingleton<IDataDirectoryPathProvider, OrganizationDataDirectoryPathProvider>()
+                .RunServiceAction(addOrganizationDirectoryPathProvider)
+                .RunServiceAction(addDataDirectoryNameConvention)
+                .RunServiceAction(addStringlyTypedPathOperator)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="OrganizationDataDirectoryPathProvider"/> implementation of <see cref="IDataDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IDataDirectoryPathProvider> AddOrganizationDataDirectoryPathProviderAction(this IServiceCollection services,
+            ServiceAction<IOrganizationDirectoryPathProvider> addOrganizationDirectoryPathProvider,
+            ServiceAction<IDataDirectoryNameConvention> addDataDirectoryNameConvention,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            var serviceAction = new ServiceAction<IDataDirectoryPathProvider>(() => services.AddOrganizationDataDirectoryPathProvider(
+                addOrganizationDirectoryPathProvider,
+                addDataDirectoryNameConvention,
+                addStringlyTypedPathOperator));
+            return serviceAction;
+        }
+    }
+}
